77ad06585b712989a92d61a2d4475a9f517d4124
adiKhan12 <ghories555@gmail.com>
2019-01-17 02:41:48 -0800

    Updates

---
+++ precache-manifest.326b5625e19836f34444017f0ecdfa93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d132a652e042fd8c1344",
+    "url": "/poe-ipfs/static/js/runtime~main.d132a652.js"
+  },
+  {
+    "revision": "0da3786dfc4f82b0f0f2",
+    "url": "/poe-ipfs/static/js/main.0da3786d.chunk.js"
+  },
+  {
+    "revision": "1728da77f11c5065cde6",
+    "url": "/poe-ipfs/static/js/1.1728da77.chunk.js"
+  },
+  {
+    "revision": "0da3786dfc4f82b0f0f2",
+    "url": "/poe-ipfs/static/css/main.7b7920c0.chunk.css"
+  },
+  {
+    "revision": "89d5e10dafceea22c93d850df83a2bdd",
+    "url": "/poe-ipfs/index.html"
+  }
+];
