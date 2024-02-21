ab60926aa0c4db88e5664d4bde57b9f3396e2dd0
Adnan Khan <Ghories555@gmail.com>
2024-02-21 17:02:17 +0100

    remove pass info

--- public/connection.php
+++ public/connection.php
@@ -1,7 +1,7 @@
 <?php
     $dbhost = "sql160.main-hosting.eu";
     $dbuser = "u889487780_mgi";
-    $dbpass = "6006255k";
+    $dbpass = "DB_PASS";
     $db     = "u889487780_hash";
 
     $conn   = new mysqli ($dbhost, $dbuser, $dbpass, $db);
